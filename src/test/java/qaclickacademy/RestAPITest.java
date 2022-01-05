d9697b1af1921ceaaaeb8a1429241dab9049cfa6
Ayman <ayman42@hotmail.com>
2022-01-05 16:29:03 -0600

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void APIApp()
+	{
+		System.out.println("APIApp");
+	}
+	
+	
+	@Test
+	public void APIIOSApp()
+	{
+		System.out.println("APIIOSApp");
+	}
+
+
+}
